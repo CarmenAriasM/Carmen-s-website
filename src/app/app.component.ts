b75ef7c03fe846a026071558f2b2028ca58dc92a
Carmen Arias <maricarmen98a@gmail.com>
2022-09-04 19:06:10 +0200

    Header light dark module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Carmen-website';
+  faSun = faSun;
+  faMoon = faMoon;
+
+  isDarkEnable = false;
+presentTheme$ = new BehaviorSubject<string>('theme-light');
+constructor() {}
+ngOnInit() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    this.presentTheme$.next(savedTheme);
+  }
+}
+changeTheme() {
+  this.presentTheme$.value === 'theme-light'
+    ? this.presentTheme$.next('theme-dark')
+    : this.presentTheme$.next('theme-light');
+  localStorage.setItem('theme', this.presentTheme$.value);
+  this.isDarkEnable = !this.isDarkEnable;
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
