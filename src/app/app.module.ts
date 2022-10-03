c87ae31d78e8e530642c718c1cabb219f00aac10
Carmen Arias <maricarmen98a@gmail.com>
2022-10-03 19:06:06 +0200

    terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ProjectsComponent } from './components/projects/projects.component';
@@ -19,7 +17,6 @@ import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
     HomeComponent,
     AboutMeComponent,
     ProjectsComponent,
